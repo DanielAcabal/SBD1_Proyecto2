2838e51a1ecb1db63c657544a5688e52d06e6a17
DanielAcabal <danchiacabal@gmail.com>
2023-04-17 00:03:04 -0600

    Crear orden y otras funciones

--- DB.sql
+++ DB.sql
@@ -47,22 +47,6 @@ CREATE TABLE direccion (
     CONSTRAINT direccion_municipio_fk FOREIGN KEY(id_municipio) REFERENCES municipio ( id_municipio )
 );
 
-CREATE TABLE empleado (
-    id_empleado                INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombres                    VARCHAR(30) NOT NULL,
-    apellidos                  VARCHAR(30) NOT NULL,
-    fecha_nacimiento           DATE NOT NULL,
-    correo                     VARCHAR(30) NOT NULL,
-    telefono                   INTEGER NOT NULL,
-    dpi                        BIGINT NOT NULL,
-    fecha_inicio               DATE NOT NULL,
-    id_puesto                  INTEGER NOT NULL,
-    id_direccion               INTEGER NOT NULL,
-    id_municipio               INTEGER NOT NULL,
-    CONSTRAINT empleado_puesto_trabajo_fk FOREIGN KEY(id_puesto) REFERENCES puesto_trabajo ( id_puesto ),
-    CONSTRAINT empleado_direccion_fk FOREIGN KEY(id_direccion,id_municipio) REFERENCES direccion( id_direccion, id_municipio)
-);
-
 CREATE TABLE restaurante (
     id_restaurante         VARCHAR(30) NOT NULL PRIMARY KEY,
     telefono               INTEGER NOT NULL,
@@ -70,11 +54,26 @@ CREATE TABLE restaurante (
     parqueo                BOOLEAN NOT NULL,
     id_direccion           INTEGER NOT NULL, 
     id_municipio           INTEGER NOT NULL,
-    id_gerente             INTEGER NOT NULL,
     CHECK ( personal > 0 ),
     CONSTRAINT restaurante_direccion_fk FOREIGN KEY(id_direccion,id_municipio) REFERENCES direccion ( id_direccion, id_municipio),
-    CONSTRAINT UC_restaurante UNIQUE (id_direccion,id_municipio),
-    CONSTRAINT restaurante_empleado_fk FOREIGN KEY(id_gerente) REFERENCES empleado(id_empleado)
+    CONSTRAINT UC_restaurante UNIQUE (id_direccion,id_municipio)
+);
+
+CREATE TABLE empleado (
+    id_empleado                INTEGER NOT NULL  AUTO_INCREMENT,
+    nombres                    VARCHAR(30) NOT NULL,
+    apellidos                  VARCHAR(30) NOT NULL,
+    fecha_nacimiento           DATE NOT NULL,
+    correo                     VARCHAR(30) NOT NULL,
+    telefono                   INTEGER NOT NULL,
+    direccion                  VARCHAR(75) NOT NULL,
+    dpi                        BIGINT NOT NULL,
+    fecha_inicio               DATE NOT NULL,
+    id_puesto                  INTEGER NOT NULL,
+    id_restaurante             VARCHAR(30) NOT NULL,
+    CONSTRAINT empleado_pk PRIMARY KEY(id_empleado,id_restaurante),
+    CONSTRAINT empleado_puesto_trabajo_fk FOREIGN KEY(id_puesto) REFERENCES puesto_trabajo ( id_puesto ),
+    CONSTRAINT empleado_restaurante_fk FOREIGN KEY(id_restaurante) REFERENCES restaurante( id_restaurante )
 );
 
 CREATE TABLE direccion_entrega (
@@ -92,12 +91,15 @@ CREATE TABLE orden (
     fecha_inicio            DATETIME NOT NULL,
     fecha_entrega           DATETIME,
     id_estado               INTEGER NOT NULL,
+    repartidor              INTEGER,
+    id_restaurante          VARCHAR(30),
     dpi_cliente             BIGINT NOT NULL,
-    id_direccion            INTEGER NOT NULL,
-    id_municipio            INTEGER NOT NULL,
+    id_direccion            INTEGER ,
+    id_municipio            INTEGER ,
     CONSTRAINT orden_estado_orden_fk FOREIGN KEY( id_estado ) REFERENCES estado_orden ( id_estado ),
     CONSTRAINT orden_entrega_fk FOREIGN KEY(dpi_cliente,id_direccion,id_municipio) 
-              REFERENCES direccion_entrega(dpi_cliente,id_direccion, id_municipio)
+              REFERENCES direccion_entrega(dpi_cliente,id_direccion, id_municipio),
+    CONSTRAINT orden_empleado_fk FOREIGN KEY(repartidor,id_restaurante) REFERENCES empleado(id_empleado,id_restaurante)
 );
 CREATE TABLE detalle_orden (
     cantidad             INTEGER NOT NULL,
@@ -121,4 +123,34 @@ CREATE TABLE factura (
     forma_pago                    CHAR(1) NOT NULL,
     CONSTRAINT factura_orden_fk FOREIGN KEY(id_orden) REFERENCES orden( id_orden )
 );
-
+-- Insert Products
+INSERT INTO producto 
+ VALUES 
+  ("C1","Cheeseburger",41.00),
+  ("C2","Chicken Sandwinch",32.00),
+  ("C3","BBQ Ribs",54.00),
+  ("C4","Pasta Alfredo",47.00),
+  ("C5","Pizza Espinator",85.00),
+  ("C6","Buffalo Wings",36.00),
+  ("E1","Papas fritas",15.00),
+  ("E2","Aros de cebolla",17.00),
+  ("E3","Coleslaw",12.00),
+  ("B1","Coca-Cola",12.00),
+  ("B2","Fanta",12.00),
+  ("B3","Sprite",12.00),
+  ("B4","Té frío",12.00),
+  ("B5","Cerveza de barril",18.00),
+  ("P1","Copa de helado",13.00),
+  ("P2","Cheesecake",15.00),
+  ("P3","Cupcake de chocolate",8.00),
+  ("P4","Flan",10.00)
+;
+-- Insert estado_orden
+INSERT INTO estado_orden (nombre)
+VALUES 
+( "INICIADA" ),
+( "AGREGANDO" ),
+( "EN CAMINO" ),
+( "ENTREGADA" ),
+( "SIN COBERTURA" )
+;
--- Funcionalidades.sql
+++ Funcionalidades.sql
@@ -1,5 +1,5 @@
 DELIMITER //
-CREATE PROCEDURE registrar_puesto(
+CREATE PROCEDURE RegistrarPuesto(
     IN nombre VARCHAR(30),
     IN descripcion VARCHAR(75),
     IN salario DECIMAL(8, 2)
@@ -9,7 +9,7 @@ BEGIN
 VALUES(nombre, descripcion, salario);
 END;
 //
-CREATE PROCEDURE registrar_cliente(
+CREATE PROCEDURE RegistrarCliente(
   IN dpi BIGINT,
   IN nombre VARCHAR(30),
   IN apellidos VARCHAR(30),
@@ -23,7 +23,7 @@ BEGIN
   VALUES ( dpi,nombre,apellidos,correo,telefono,nit,nacimiento );
 END;
 //
-CREATE PROCEDURE registrar_direccion(
+CREATE PROCEDURE RegistrarDireccion(
   IN dpi BIGINT,
   IN direccion VARCHAR(75),
   IN municipio VARCHAR(30),
@@ -38,7 +38,7 @@ BEGIN
     VALUES (dpi,LAST_INSERT_ID(), muni);
 END;
 //
-CREATE PROCEDURE registrar_empleado(
+CREATE PROCEDURE CrearEmpleado(
   IN nombres    VARCHAR(30),
   IN apellidos  VARCHAR(30),
   IN nacimiento DATE,
@@ -47,48 +47,108 @@ CREATE PROCEDURE registrar_empleado(
   IN direccion  VARCHAR(75),
   IN dpi        BIGINT,
   IN puesto     INTEGER,
-  IN inicio     DATE
+  IN inicio     DATE,
+  IN restaurante VARCHAR(30)
 )
 BEGIN
   INSERT INTO empleado (nombres,apellidos,fecha_nacimiento,
-                        correo,telefono,dpi,fecha_inicio,id_puesto,
-                        id_direccion,id_municipio)
-    VALUES (nombres,apellidos,nacimiento,correo,telefono,dpi,inicio,
-            puesto,1,1);
+                        correo,telefono,direccion,dpi,fecha_inicio,id_puesto,
+                        id_restaurante)
+    VALUES (nombres,apellidos,nacimiento,correo,telefono,direccion,dpi,inicio,
+            puesto,restaurante);
 END;
 //
-CREATE PROCEDURE registrar_restaurante(
+CREATE PROCEDURE RegistrarRestaurante(
   IN id         VARCHAR(30),
   IN direccion  VARCHAR(75),
   IN municipio  VARCHAR(30),
   IN zona       INTEGER,
   IN telefono   INTEGER,
   IN personal   INTEGER,
-  IN parqueo    BOOLEAN,
-  IN gerente    INTEGER
+  IN parqueo    BOOLEAN
 )
 BEGIN
   DECLARE muni INTEGER;
   INSERT IGNORE INTO municipio (nombre) VALUES (municipio);
   SELECT m.id_municipio into muni FROM municipio m WHERE m.nombre=municipio;
   INSERT INTO direccion (direccion,zona,id_municipio) VALUES (direccion,zona,muni);
-  INSERT INTO restaurante VALUES (id,telefono,personal,parqueo,LAST_INSERT_ID(), muni,gerente);
+  INSERT INTO restaurante VALUES (id,telefono,personal,parqueo,LAST_INSERT_ID(), muni);
 END;
 
 //
-CREATE PROCEDURE crear_orden(
+CREATE FUNCTION getEstado(estado VARCHAR(20))
+RETURNS INTEGER
+BEGIN
+  RETURN
+  SELECT e.id_estado FROM estado_orden e WHERE e.nombre=estado;
+END
+//
+CREATE PROCEDURE CrearOrden(
   IN dpi        BIGINT,
   IN direccion  INTEGER,
-  IN canal      CHAR(2)
+  IN canal      CHAR(1)
 )
-BEGIN
-  DECLARE estado INTEGER;
-  DECLARE muni INTEGER;
-  SELECT e.id_estado INTO estado FROM estado_orden e WHERE e.nombre="INICIADA";
-  SELECT e.id_municipio INTO muni FROM direccion_entrega e WHERE e.id_direccion=direccion;
- INSERT INTO orden (
-  canal,fecha_inicio,fecha_entrega,id_estado,dpi_cliente,id_direccion,id_municipio
+orden:BEGIN
+  DECLARE muni,direc INTEGER;
+  DECLARE res VARCHAR(30);
+  IF NOT existe_cliente(dpi) THEN
+    SELECT CONCAT('No existe el cliente ',dpi) AS ERROR;
+    LEAVE orden;
+  END IF;
+  IF check_with_regex(canal,'L|A') THEN
+    SELECT 'Solamente puede usar canal L o A' AS ERROR;
+    LEAVE orden;
+  END IF;
+  CALL datos_cobertura(dpi,res);
+  IF res IS NULL THEN
+    SELECT 'Sin cobertura' AS ERROR;
+  INSERT INTO orden (
+  canal,fecha_inicio,fecha_entrega,id_estado,dpi_cliente,id_direccion,id_municipio,id_restaurante
  ) VALUES 
-  ( canal, NOW(), NULL, estado, dpi, direccion, muni); -- Revisar xd 
+  ( canal, NOW(), NULL, getEstado("SIN COBERTURA"), dpi, direccion, muni, res); -- Revisar xd 
+  LEAVE orden;
+  END IF;
+  SELECT id_direccion,id_municipio INTO direc,muni FROM direccion_entrega WHERE dpi_cliente=dpi AND id_direccion=direccion;
+  IF direc IS NULL OR muni IS NULL THEN
+    SELECT 'Esta direccion no la tiene registrada' AS ERROR;
+    LEAVE orden;
+  END IF;
+ INSERT INTO orden (
+  canal,fecha_inicio,fecha_entrega,id_estado,dpi_cliente,id_direccion,id_municipio,id_restaurante
+ ) VALUES 
+  ( canal, NOW(), NULL, getEstado("INICIADA"), dpi, direc, muni, res); -- Revisar xd 
 END;
+//
+DELIMITER //
+CREATE PROCEDURE datos_cobertura(dpi BIGINT, OUT res VARCHAR(30))
+BEGIN
+SELECT t.id_restaurante INTO res FROM ( SELECT r.id_restaurante,d.zona,d.id_municipio FROM restaurante r
+  JOIN direccion d ON r.id_direccion=d.id_direccion) t
+  JOIN (
+  SELECT d1.*,d.dpi_cliente FROM direccion_entrega d 
+  JOIN direccion d1 ON d.id_direccion=d1.id_direccion
+  WHERE d.dpi_cliente=dpi) t1
+  ON t.zona=t1.zona AND t.id_municipio=t1.id_municipio
+  LIMIT 1;
+END
+//
+DROP PROCEDURE IF EXISTS datos_cobertura;
+DROP PROCEDURE IF EXISTS CrearOrden;
+DELIMITER //
+CREATE FUNCTION check_with_regex( txt VARCHAR(100), regex VARCHAR(100))
+RETURNS BOOLEAN
+BEGIN
+	DECLARE ok BOOLEAN DEFAULT FALSE;
+    IF (SELECT REGEXP_INSTR(txt,regex)=0) THEN
+     SELECT TRUE INTO ok;
+    END IF;
+    RETURN (ok);
+END
+//
+DELIMITER //
+CREATE FUNCTION existe_cliente( dpi BIGINT )
+RETURNS BOOLEAN
+BEGIN
+	RETURN  (SELECT EXISTS(SELECT 1  FROM cliente WHERE dpi_cliente=dpi));
+END
 //
